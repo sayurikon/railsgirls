7b60f36f23d5bc65cd7580b10449ca1c633f4bf0
sayurikon <sayuri.k@wamii.co.jp>
2025-02-22 16:17:41 +0900

    コメント追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_015904) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_22_070103) do
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
+
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_22_015904) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "ideas"
 end
